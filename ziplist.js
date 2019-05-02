2296260b34627ce13f3805bb9236ba5df619d777
martyjapilado <martyjapilado@gmail.com>
2019-05-01 17:06:25 -1000

    Fixed some Eslint errors

--- ziplist.js
+++ ziplist.js
@@ -2,7 +2,7 @@ const testList1 = ['a', 'b', 'c'];
 const testList2 = ['1', '2', '3'];
 
 function ziplist(list1, list2) {
-  let result = [];
+  const result = [];
   for (let i = 0; i < list1.length || i < list2.length; i++) {
     result.push(list1[i]);
     result.push(list2[i]);
@@ -13,5 +13,5 @@ function ziplist(list1, list2) {
 function ziplistTheSimpleWay(list1, list2) {
   return _.flatten(_.zip(list1, list2));
 }
-console.log(this.ziplistTheSimpleWay(testList1, testList2));
-console.log(this.ziplist(testList1, testList2));
+console.log(ziplistTheSimpleWay(testList1, testList2));
+console.log(ziplist(testList1, testList2));
